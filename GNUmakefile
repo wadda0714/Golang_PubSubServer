b2090196637b21629d076e01be83039432aa7dc4
和田皓翔 <wadahiroka@wadaakirashounoMacBook-Pro.local>
2023-05-12 15:06:10 +0900

    fix

--- GNUmakefile
+++ GNUmakefile
@@ -2,7 +2,7 @@ gen-design:
 	@echo "Generating design..."
 	@goa gen github.com/wadda0714/Golang_PubSubServer/server/design -o ./server 
 gen-example:
-	#注意！自分でで実装した部分は消えます　
+	#注意！自分で実装した部分は消えます　
 	@echo "Generating example..."
 	@cp ./server/pub_sub_server.go ./backup/pub_sub_server.go
 	@rm  ./server/pub_sub_server.go
--- util/file.go
+++ util/file.go
@@ -1,6 +1,8 @@
 package util
 
 import (
+	"bufio"
+	"io"
 	"os"
 )
 
@@ -30,5 +32,36 @@ func IsFileExists(path string) bool {
 		}
 	}
 	return true
+
+}
+
+func ReadFile(path string) ([]string, error) {
+
+	f, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	reader := bufio.NewReaderSize(f, 64)
+
+	var nameList []string
+	var tmp []byte
+
+	for {
+		line, _, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		tmp = append(tmp, line...)
+		nameList = append(nameList, string(tmp))
+		tmp = []byte{}
+
+	}
+
+	return nameList, nil
 
 }
